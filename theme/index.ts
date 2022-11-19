06c1f5d746fd681b5722f6ed0ad6a0e5dd7182bd
Sunrit Jana <warriordefenderz@gmail.com>
2022-11-19 17:58:02 +0530

    Setup the project and add basic UI

---
+++ theme/index.ts
@@ -0,0 +1,9 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+  colors: {
+    background: '#1F1F1F',
+    accent: '#833BBE',
+    bodyText: 'rgba(255, 255, 255, 0.75)'
+  }
+});
